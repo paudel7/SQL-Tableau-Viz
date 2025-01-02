baa7ab98b5a85e388fd50886c720b1f9f9a49ed4
kiran p <43019661+paudel7@users.noreply.github.com>
2025-01-02 13:50:57 -0500

    files uploaded

---
+++ SQLQuery_datacheck.sql
@@ -0,0 +1,12 @@
+/* Verify the data */
+select * from sales.customers;
+select * from sales.order_items;
+select * from sales.orders;
+select * from sales.staffs;
+select * from sales.stores;
+select * from production.brands;
+select * from production.categories;
+select * from production.products;
+select * from production.stocks;
+
+
